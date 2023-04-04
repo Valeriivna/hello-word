2989be8f8924798b9506ecea68adda9b84f1971c
Viktoria <127214157+Valeriivna@users.noreply.github.com>
2023-04-04 10:03:34 +0300

    add new project

---
+++ ОПІ_ЛР_2/ОПІ_ЛР_2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ОПІ_ЛР_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = a / 4 * Math.Pow(b, 2) * (1 / Math.Tan(Math.PI / a));
+            textBox3.Text = Convert.ToString(c);
+        }
+    }
+    }
